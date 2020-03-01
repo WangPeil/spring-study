60931b081a9d5fe4ef8e49a789edfa93ca976b87
peili.wang <peili.wang@qunar.com>
2020-03-02 00:09:17 +0800

    Spring AOP  XML配置和注解配置

--- spring-mvc/src/main/java/com/example/config/MyConfig.java
+++ spring-mvc/src/main/java/com/example/config/MyConfig.java
@@ -2,10 +2,11 @@ package com.example.config;
 
 import com.example.pojo.User;
 import com.google.common.collect.Lists;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
+import org.springframework.context.annotation.Scope;
 
 /**
  * 表明这是一个配置类
@@ -14,15 +15,18 @@ import java.util.ArrayList;
  * @date 2020/2/24 18:09
  */
 @Configuration
+@ComponentScan("com.example.pojo")
 public class MyConfig {
 
     /**
      * 返回User的实例
+     *
      * @return user
      */
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public User getUser() {
-        return new User(1,"aaa",3, Lists.newArrayList("书1", "书2"));
+        return new User(1, "aaa", 3, Lists.newArrayList("书1", "书2"));
     }
 
 }
